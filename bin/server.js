848ed337d3d8c05cf1f7c134dce1dbf0cf6a95da
Letty <dittrich.katja@googlemail.com>
2015-12-12 00:57:18 +0100

    change objects to array objects

--- bin/server.js
+++ bin/server.js
@@ -24,6 +24,7 @@ async.parallel([
 	var l_ = results[0];
 	var s_ = results[1];
 	var lookup = {};
+	var ar = [];
 
 	//geokoordinate 24 u 25
 	var lifts = l_.split('\n');
@@ -31,10 +32,12 @@ async.parallel([
 		var lift = lifts[i].split('\t');
 		if (lift[24] === '' || lift[25] === '') {
 			if (!lookup[lift[5]]) {
-				lookup[lift[5]] = {
+				var o = lookup[lift[5]] = {
 					station_id: lift[5],
 					lifts: []
 				};
+
+				ar.push(lift[5]);
 			}
 			lookup[lift[5]].lifts.push({
 				equipment_id: lift[2]
@@ -53,12 +56,16 @@ async.parallel([
 		var station = stations[i].split('\,');
 		if (lookup[station[2]]) {
 			lookup[station[2]].name = station[3];
-			lookup[station[2]].long = station[14];
-			lookup[station[2]].lat = station[13];
+			lookup[station[2]].lng = parseFloat(station[14]);
+			lookup[station[2]].lat = parseFloat(station[13]);
 		}
 	}
+	var a = [];
+	ar.forEach(function (id) {
+		a.push(lookup[id]);
+	});
 
-	GeoJSON.parse([lookup], {Point: ['lat', 'long']}, function (geojson) {
+	GeoJSON.parse(a, {Point: ['lat', 'lng']}, function (geojson) {
 		fs.writeFile('missingPoints.geojson', JSON.stringify(geojson, null, 3), 'utf8', function (err) {
 			if(err) throw err;
 			console.log('done');
