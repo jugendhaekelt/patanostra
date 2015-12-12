7e3bc8d9908887d500779479e7f04aa24483f2e9
Knut Perseke <knut.perseke@gmail.com>
2015-12-12 03:39:03 +0100

    add geojson for falid tagged lifts

--- bin/server.js
+++ bin/server.js
@@ -24,7 +24,9 @@ async.parallel([
 	var l_ = results[0];
 	var s_ = results[1];
 	var lookup = {};
+	var lookup_tagged = {};
 	var ar = [];
+	var ar_valid = [];
 
 	//geokoordinate 24 u 25
 	var lifts = l_.split('\n');
@@ -40,6 +42,18 @@ async.parallel([
 				ar.push(lift[5]);
 			}
 			lookup[lift[5]].lifts.push({
+				equipment_id: lift[2]
+			});
+		} else {
+			if (!lookup_tagged[lift[5]]) {
+				var o = lookup_tagged[lift[5]] = {
+					station_id: lift[5],
+					lifts: []
+				};
+
+				ar_valid.push(lift[5]);
+			}
+			lookup_tagged[lift[5]].lifts.push({
 				equipment_id: lift[2]
 			});
 		}
@@ -58,11 +72,16 @@ async.parallel([
 			lookup[station[2]].name = station[3];
 			lookup[station[2]].lng = parseFloat(station[14]);
 			lookup[station[2]].lat = parseFloat(station[13]);
-
+		}
+		if (lookup_tagged[station[2]]) {
+			lookup_tagged[station[2]].name = station[3];
+			lookup_tagged[station[2]].lng = parseFloat(station[14]);
+			lookup_tagged[station[2]].lat = parseFloat(station[13]);
 		}
 	}
 
 	var a = [];
+	var a_tagged = [];
 	ar.forEach(function (id) {
 		var current = lookup[id];
 		if (!isNaN(current.lng) && !isNaN(current.lat)) {
@@ -73,10 +92,27 @@ async.parallel([
 		}
 	});
 
+	ar_valid.forEach(function (id) {
+		var current = lookup_tagged[id];
+		if (!isNaN(current.lng) && !isNaN(current.lat)) {
+			a_tagged.push(lookup_tagged[id]);
+		}
+		else {
+			console.error("Can't parse Station for id: " + current.station_id);
+		}
+	});
+
 	console.log("Checking...");
 
 	GeoJSON.parse(a, {Point: ['lat', 'lng']}, function (geojson) {
 		fs.writeFile('missingPoints.geojson', JSON.stringify(geojson, null, 3), 'utf8', function (err) {
+			if(err) throw err;
+			console.log('done');
+		});
+	});
+
+	GeoJSON.parse(a_tagged, {Point: ['lat', 'lng']}, function (geojson) {
+		fs.writeFile('validPoints.geojson', JSON.stringify(geojson, null, 3), 'utf8', function (err) {
 			if(err) throw err;
 			console.log('done');
 		});
