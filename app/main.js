c096e8d7496069f9397ba629f1090db99e8fbcdd
Knut Perseke <knut.perseke@gmail.com>
2015-12-12 05:23:28 +0100

    new main.js for js code + slider for click on pin + different colors for pins

---
+++ app/main.js
@@ -0,0 +1,108 @@
+$(document).ready(function() {
+    var map = L.map('map').setView([52, 10], 7);
+    var activePin = -1;
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6IjZjNmRjNzk3ZmE2MTcwOTEwMGY0MzU3YjUzOWFmNWZhIn0.Y8bhBaUMqFiPrDRW9hieoQ', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        id: 'mapbox.light'
+    }).addTo(map);
+
+    var geojsonMarkerOptions = {
+        radius: 8,
+        fillColor: "#ff0000",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    var geojsonMarkerOptions_valid = {
+        radius: 8,
+        fillColor: "#00ff00",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    function onEachFeature(feature, layer) {
+        var popupContent = "<strong>" + feature.properties.name + "</strong><br />";
+
+        //feature.properties.lifts.forEach(function(item){
+        //    popupContent += "Aufzug: " + item.equipment_id + "<br />";
+        //});
+        //
+        //if (feature.properties && feature.properties.popupContent) {
+        //    popupContent += feature.properties.popupContent;
+        //}
+        //
+        //layer.bindPopup(popupContent);
+
+        layer.on({
+            click: function(pin) {
+                pinId = pin.target._leaflet_id;
+
+                if (pinId === activePin) {
+
+                    $('.infobox').addClass('infobox-slide-out');
+                    $('.infobox').removeClass('infobox-slide-in');
+                    activePin = -1;
+                } else {
+                    $('.infobox').removeClass('infobox-slide-out');
+                    $('.infobox').addClass('infobox-slide-in');
+                    activePin = pinId;
+                    var popupContent = '';
+                    var feature = pin.target.feature.properties;
+
+                    if (feature.lifts)
+                        feature.lifts.forEach(function(item){
+                            popupContent += "Aufzug: " + item.equipment_id + "<br />";
+                    });
+
+                    if (feature.popupContent) {
+                        popupContent += feature.properties.popupContent;
+                    }
+                    console.log(feature);
+                    console.log(popupContent);
+                }
+
+                console.log("Active Pin: " + pin.target._leaflet_id);
+            }
+        });
+    }
+
+
+    var station_points = L.geoJson(stations, {
+        onEachFeature: onEachFeature,
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, geojsonMarkerOptions);
+        }
+    });
+    station_points.addTo(map);
+
+    var station_points_tagged = L.geoJson(stations_valid, {
+        onEachFeature: onEachFeature,
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, geojsonMarkerOptions_valid);
+        }
+    });
+    station_points_tagged.addTo(map);
+
+    var baseLayers = {};
+
+    var overlays = {
+        "Untagged": station_points,
+        "Tagged": station_points_tagged
+    };
+
+    L.control.layers(baseLayers, overlays).addTo(map);
+
+    map.on('click', function() {
+        $('.infobox').addClass('infobox-slide-out');
+        $('.infobox').removeClass('infobox-slide-in');
+        activePin = -1;
+    });
+});
